94a915df222031339179210e49c766da79b0b4d2
raulescobar-g <raulescobar_g@tamu.edu>
2022-09-01 16:12:32 -0500

    saving

---
+++ Ball/build/CMakeFiles/Ball.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/Camera.cpp" "CMakeFiles/Ball.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/Camera.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/GLSL.cpp" "CMakeFiles/Ball.dir/src/GLSL.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/GLSL.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/Material.cpp" "CMakeFiles/Ball.dir/src/Material.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/Material.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/MatrixStack.cpp" "CMakeFiles/Ball.dir/src/MatrixStack.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/MatrixStack.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/Program.cpp" "CMakeFiles/Ball.dir/src/Program.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/Program.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/Shape.cpp" "CMakeFiles/Ball.dir/src/Shape.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/Shape.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/Texture.cpp" "CMakeFiles/Ball.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/Texture.cpp.o.d"
+  "/Users/raulescobar/Documents/CSCE_649/Ball/src/main.cpp" "CMakeFiles/Ball.dir/src/main.cpp.o" "gcc" "CMakeFiles/Ball.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/raulescobar/Documents/glfw-3.3.6/debug/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ball/build/CMakeFiles/Ball.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "Ball"
+  "Ball.pdb"
+  "CMakeFiles/Ball.dir/src/Camera.cpp.o"
+  "CMakeFiles/Ball.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/GLSL.cpp.o"
+  "CMakeFiles/Ball.dir/src/GLSL.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/Material.cpp.o"
+  "CMakeFiles/Ball.dir/src/Material.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/MatrixStack.cpp.o"
+  "CMakeFiles/Ball.dir/src/MatrixStack.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/Program.cpp.o"
+  "CMakeFiles/Ball.dir/src/Program.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/Shape.cpp.o"
+  "CMakeFiles/Ball.dir/src/Shape.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/Texture.cpp.o"
+  "CMakeFiles/Ball.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/Ball.dir/src/main.cpp.o"
+  "CMakeFiles/Ball.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
